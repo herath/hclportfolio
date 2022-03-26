c21e04a3f0aa72c77fb3d8d235c8ae8e82efd2f1
shafshafrin <102394208+shafshafrin@users.noreply.github.com>
2022-03-26 12:20:02 +0530

    Create AssetDetails.java

---
+++ Portfolio/src/main/java/com/portfolio/api/Portfolio/Persistence/Entity/AssetDetails.java
@@ -0,0 +1,51 @@
+package com.portfolio.persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class AssetDetails {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String assetName;
+
+    private Long unitPrice;
+
+    private Long remQuantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAssetName() {
+        return assetName;
+    }
+
+    public void setAssetName(String assetName) {
+        this.assetName = assetName;
+    }
+
+    public Long getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Long unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Long getRemQuantity() {
+        return remQuantity;
+    }
+
+    public void setRemQuantity(Long remQuantity) {
+        this.remQuantity = remQuantity;
+    }
+}
