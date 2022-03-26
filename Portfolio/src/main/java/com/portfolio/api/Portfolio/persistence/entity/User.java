d08393aea72a02ee2e2cfa97f2818bcd3ded5f87
thilinamu <thilinamu@mobitel.lk>
2022-03-26 13:22:04 +0530

    Add auditable.

--- Portfolio/src/main/java/com/portfolio/api/Portfolio/persistence/entity/User.java
+++ Portfolio/src/main/java/com/portfolio/api/Portfolio/persistence/entity/User.java
@@ -14,7 +14,7 @@ public class User extends Auditable<String> implements Serializable {
 	@Id
 	@GeneratedValue
 	@Column(name = "id", unique = true, nullable=false)
-	private UUID id;
+	private Long id;
 
 	@Column(name = "username", unique = true, nullable=false)
 	private String username;
@@ -22,11 +22,11 @@ public class User extends Auditable<String> implements Serializable {
 	@Column(name = "password")
 	private String password;
 
-	public UUID getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(UUID id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
